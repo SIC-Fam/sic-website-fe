cc75e65521fb01aa8ef0934263b0e74b9a7fa71f
Bùi Thúy Ngọc <90141097+ngoc2003@users.noreply.github.com>
2023-07-03 12:23:56 +0700

    feat: Config main base of project (#1)

---
+++ src/pages/_document.tsx
@@ -0,0 +1,29 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:description" content="Sinh Viên Giúp Sinh Viên Làm Chủ Công Nghệ" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="627" />
+        <link rel="icon" href="/images/logo.png" />
+        <link rel="apple-touch-icon" href="/images/logo.png" />
+        <link rel="image_src" href="/images/logo.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600&display=swap"
+          rel="stylesheet"
+        />
+        <title>SIC - Student Informatic Club</title>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
