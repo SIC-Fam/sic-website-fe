98b0ad62cd7f8de673ebc7833dac6c9b729aaa7a
minh_dev <75151777+minhdev-ui@users.noreply.github.com>
2023-07-04 18:38:37 +0700

    feat: Add components (#3)

---
+++ src/components/icons/ChevronDown.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChevronDown = (props) => {
+  return (
+    <svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+    </svg>
+  );
+};
+
+export default ChevronDown;
