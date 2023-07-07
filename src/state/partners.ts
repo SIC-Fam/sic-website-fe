8293c6ce43d5594f5bbdd79768ad36ae4e06c4b8
Bùi Thúy Ngọc <90141097+ngoc2003@users.noreply.github.com>
2023-07-07 19:34:32 +0700

    feat: update data of partners (#11)

--- src/state/partners.ts
+++ src/state/partners.ts
@@ -8,12 +8,12 @@ const partnersAtom = atom({
       body: '-> ICDL Vietnam is a member of the ICDL Foundation - the global management and administration organization of the ICDL international standard IT skills certificates. The certificate has been recognized in more than 100 countries and territories, in 41 languages ​​and more than 24,000 test centers around the world.',
     },
     {
-      header: 'Texted 1',
-      body: 'Lorem 1',
+      header: 'Laptop AZ',
+      body: '-> LaptopAZ is a laptop brand specializing in selling laptops and genuine laptop components. With many years of experience in the industry and a dedicated team of professional engineers, LaptopAZ always strives to bring customers the best quality products, excellent warranty and support services, and competitive prices that are worth the value for money invested by customers.',
     },
     {
-      header: 'Texted 1',
-      body: 'Lorem 1',
+      header: 'BIT - BAV Club',
+      body: '-> BIT - BAV is an academic club focused on Information Technology, under the Student Association of the Banking Academy. It is both a partner and a friendly neighbor of SIC. SIC and BIT always support and accompany each other in various events, big and small.',
     },
   ],
 });
